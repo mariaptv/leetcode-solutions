c1078d0ea7ae42980511ac05849b10478dfbc0e3
maria.tellez <maria.tellez@sezzle.com>
2025-06-08 20:48:28 -0500

    Add two sum exercise

---
+++ Exercise/twosum.py
@@ -0,0 +1,16 @@
+def two_sum(nums, target):
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+            
+def main():
+    nums = [2, 7, 11, 15]
+    target = 9
+    result = two_sum(nums, target)
+    print(f"Indices of the two numbers that add up to {target}: {result}")
+
+if __name__ == "__main__":
+    main()
+
+
